f6aecf6b7e54b8c6fe386b56b9d86705cc7ec16c
Robert Schwentker <schwentker@gmail.com>
2025-08-21 12:43:19 -0700

    Fix basePath for internal navigation

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/srm-chatbot',
-  assetPrefix: '/srm-chatbot/',
+  assetPrefix: '/srm-chatbot',
   trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true
   }
 }
-
 module.exports = nextConfig
