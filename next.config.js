b50c50e2e2b85d361be3376fd52552bf6f708f9b
Robert Schwentker <schwentker@gmail.com>
2025-08-21 12:57:37 -0700

    Remove basePath to fix original deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/srm-chatbot',
-  assetPrefix: '/srm-chatbot',
   trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true
   }
 }
+
 module.exports = nextConfig
